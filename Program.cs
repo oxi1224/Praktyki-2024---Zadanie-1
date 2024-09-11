7bc28d2b93b9cd6a3adc3ba5ce9b927185a841c5
oxi1224 <oxi1224@o2.pl>
2024-09-11 16:59:55 +0200

    Skończenie zadania, zapoznianie się z językiem c#

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿FileStream f = File.Open("C:\\test\\test_ale_woj.txt", FileMode.Open);
+StreamReader sr = new(f);
+
+int liczba_a = 0;
+foreach (char c in sr.ReadToEnd()) {
+  if (c == 'a') liczba_a++;
+}
+Console.WriteLine($"Liczba \"a\" w pliku: {liczba_a}");
+sr.Close();
+f.Close();
